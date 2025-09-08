26079520c2b62d91a0693480c3ac93c1905c1b75
Tobi Ogunfemi <108277167+OTobi21@users.noreply.github.com>
2025-09-08 13:26:15 +0100

    first commit

---
+++ test/src/App.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import Search from "./components/search";
+import Loading from "./components/loading";
+import MovieCard from "./components/movieCard";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+const App = () => {
+  const [searchTerm, setsearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movielist, setMovielist] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+
+      if (data.response === "false") {
+        setErrorMessage(data.error || "failed to fetch movies");
+        setMovielist([]);
+        return;
+      }
+      setMovielist(data.results || []);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setErrorMessage("Failed to fetch movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const filteredMovies = movielist.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./hero.png" alt="hero banner" />
+            <h1>
+              Find <span className="text-gradient">Movies</span> You Will Enjoy
+              Withot the Hassle
+            </h1>
+            <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
+          </header>
+          <section className="all-movies">
+            <h2 className="mt-[40px]">All Movies</h2>
+            {isLoading ? (
+              <Loading />
+            ) : errorMessage ? (
+              <p className="error-message">{errorMessage}</p>
+            ) : (
+              <ul>
+                {filteredMovies.map((movie) => (
+                  <MovieCard key={movie.id} movie={movie} />
+                ))}
+              </ul>
+            )}
+          </section>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ test/src/components/MovieCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+const MovieCard = ({ movie }) => {
+  const { title, vote_average, poster_path, original_language, release_date, original_title } =
+    movie;
+  return (
+    <div className="movie-card">
+      <img
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+            : "/No-Poster.png"
+        }
+      />
+
+      <div className="mt-4">
+        <h3>{title}</h3>
+
+        <div className="content">
+
+            <div className="rating">
+                <img src="./Rating.svg" alt="star icon" />
+                <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+
+                <span>.</span>
+                <p className="lang">{original_language}</p>
+                <span>.</span>
+                <p className="year">{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+                <span>.</span>
+                <p className="lang">{original_language}</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MovieCard;
